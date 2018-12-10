c1b16f6d891a57be387155dab7c276809616c9eb
926735 <926735@2682IR30213380B.ad1.peelsb.com>
2018-12-10 14:45:32 -0500

    Removed cameramanager, cleaned up Game from unnecessary methods

--- LIBGDX Game/src/Game.java
+++ LIBGDX Game/src/Game.java
@@ -1,9 +1,9 @@
+import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.ApplicationListener;
 
 import core.Renderer;
 
-public class Game implements ApplicationListener{
-
+public class Game extends ApplicationAdapter{
 	private Renderer renderer;
 	
 	public Game()
@@ -13,40 +13,16 @@ public class Game implements ApplicationListener{
 	
 	@Override
 	public void create() {
-		// TODO Auto-generated method stub
 		renderer.init();
 	}
 
 	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
 		renderer.cleanUp();
 	}
 
 	@Override
-	public void pause() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
 	public void render() {
-		// TODO Auto-generated method stub
 		renderer.render();
 	}
-
-	@Override
-	public void resize(int arg0, int arg1) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void resume() {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	
-
 }
